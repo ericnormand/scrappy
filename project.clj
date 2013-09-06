8796b5db90c2f3b1a8c6417bbe08bb1221f25b80
Neil Mock <neilmock@gmail.com>
2013-09-06 10:45:51 -0500

    initial

---
+++ project.clj
@@ -0,0 +1,12 @@
+(defproject com.secondchance/scrappy "0.0.1-SNAPSHOT"
+  :description "Let me at em!"
+
+  :dependencies [[org.clojure/clojure "1.5.1"]
+                 [org.jsoup/jsoup "1.7.2"]]
+
+  :plugins [[s3-wagon-private "1.1.2"]]
+
+  :repositories {"secondchance" {:url "s3p://maven.secondchance/releases/"
+                                 :username ~(System/getenv "AWS_ACCESS_KEY")
+                                 :passphrase ~(System/getenv "AWS_SECRET_KEY")
+                                 :sign-releases false}})
